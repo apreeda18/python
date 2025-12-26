dcefbc26d898ba0c49d804b8c102552b75e0505f
apreeda dhakal <apreeda.ace081bct018@acem.edu.np>
2025-12-26 13:50:49 +0545

    Add files via upload

---
+++ text.py
@@ -0,0 +1,27 @@
+# name=input("enter your name")
+# print(name)
+# a=int(input("enter a number"))
+# print(type(a))
+# b=int(input("enter a number"))
+# print(type(b))
+# sum=a+b
+# print(sum)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(type(a>=b))
+# a=True
+# b=False
+# print(a and b)
+# print(a or b)
+# print(not b)
+# num= int(input("enter a numbers"))
+# if(num>0 and num<50):
+#     print("smaller")
+# elif(num>50 and num<100):
+#     print("larger")
+# else:
+#     print("exact")  
+# for i in range (10):
+#     print(i)
